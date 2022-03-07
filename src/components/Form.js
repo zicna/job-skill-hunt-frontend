8148ecdd27d00b6b3e341c1ccb9ef7a88313bcd7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-07 14:15:54 -0600

    add fetch to handle submit method

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,82 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
-    state ={
-        first_name: "",
-        last_name: "",
-        email: "",
-        password: ""
-    }
+  state = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+  }
 
-    handleChange = (event) => {
-        // console.log("change has been made")
-        this.setState({[event.target.name]: event.target.value})
-    }
+  handleChange = (event) => {
+    // console.log("change has been made")
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-    handleSubmit(event){
-        event.preventDefault()
-        // some code here
-        this.setState({first_name: "",
-        last_name: "",
-        email: "",
-        password: ""})
-    }
+  handleSubmit(event) {
+    event.preventDefault()
+    // some code here
+    // debugger
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': true,
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then((response) => {
+        debugger  
+        response.json()})
+      .then((data) => console.log(data))
+
+    //   reset form
+    this.setState({ first_name: '', last_name: '', email: '', password: '' })
+  }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
-          <div>
-              <input type="text" value= {this.state.first_name} onChange={this.handleChange} name="first_name" placeholder='first_name' />
-          </div>
-          <div>
-              <input type="text" value= {this.state.last_name} onChange={this.handleChange} name="last_name" placeholder='last_name' />
-          </div>
-          <div>
-              <input type="email" value= {this.state.email} onChange={this.handleChange} name="email" placeholder='email' />
-          </div>
-          <div>
-              <input type="password" value= {this.state.password} onChange={this.handleChange} name="password" placeholder='password' />
-          </div>
-          <div>
-              <input type="submit" value="Submit" />
-          </div>
+        <div>
+          <input
+            type="text"
+            value={this.state.first_name}
+            onChange={this.handleChange}
+            name="first_name"
+            placeholder="first_name"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={this.state.last_name}
+            onChange={this.handleChange}
+            name="last_name"
+            placeholder="last_name"
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            name="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            name="password"
+            placeholder="password"
+          />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
       </form>
     )
   }
