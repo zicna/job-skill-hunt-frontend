be4b6b5a3d164da7b0e66853eba3f4456a57b415
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-14 12:03:49 -0500

    add fetch for logout request

--- src/components/forms/SignOutForm.js
+++ src/components/forms/SignOutForm.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react'
 
-const SignOutForm = ({ conditionalRender }) => {
-  console.log(conditionalRender)
-  const [signOut, setSignOut] = useState(false)
+const SignOutForm = () => {
+
+  const handleLogOut = () => {
+    fetch('http://localhost:3001/logout', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          return response.json().then((json) => Promise.reject(json))
+        }
+      })
+      .then((json) => {
+        //   debugger
+        console.log(json)
+        localStorage.clear()
+      })
+      .catch((err) => {
+          debugger
+          console.log(err)
+        })
+  }
 
   return (
     <div>
-      <button onClick={() => conditionalRender()}>Sign me out</button>
+      <button onClick={handleLogOut}>Sign me out</button>
     </div>
   )
 }
