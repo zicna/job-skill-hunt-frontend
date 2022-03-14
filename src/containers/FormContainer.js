05d0823f7c7d46c24b937c813ec44c65cb67c084
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-14 10:53:11 -0500

    add method and pass it to the components

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -1,5 +1,5 @@
 // import { render } from '@testing-library/react'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import SignUpForm from '../components/forms/SignUpForm'
 import SignOutForm from '../components/forms/SignOutForm'
 
@@ -7,15 +7,23 @@ import SignOutForm from '../components/forms/SignOutForm'
 export default function FormContainer() {
 
 
-    const [a, setA ] = useState(()=> {
+    const [render, setRender ] = useState(()=> {
         return !!localStorage.token
     })
 
+    const conditionalRender = () => {
+        setRender(!render)
+    }
+
+    useEffect(() => {
+        console.log("render")
+    },[render])
+
     const renderForms = () => {
-        if (a){
-            return < SignOutForm/>
+        if (render){
+            return < SignOutForm props = {conditionalRender}/>
         }else{
-            return <SignUpForm />
+            return <SignUpForm props = {conditionalRender}/>
         }
     }
     
