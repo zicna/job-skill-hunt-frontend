213c6f8dd56245be1d50746006c571da137be070
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-15 16:53:03 -0500

    add new component to practise

---
+++ src/pages/AboutClass.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AboutClass extends Component {
+
+    componentDidMount(){
+        console.log("component did mount")
+    }
+  render() {
+    return (
+      <div>AboutClass</div>
+    )
+  }
+}
