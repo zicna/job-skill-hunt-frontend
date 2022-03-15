d920557e0240591bdaae6c11885c3f75b07f3e6d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-15 16:52:19 -0500

    4 ways to render components with Route from react-router-dom

--- src/containers/RoutesContainer.js
+++ src/containers/RoutesContainer.js
@@ -6,15 +6,19 @@ import Home from '../pages/Home'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
 import Legacy from '../pages/Legacy'
+import AboutClass from '../pages/AboutClass'
 
 
 const RoutesContainer = () => {
 return (
     <Switch>
-        <Route exact path="/" component={<Home/>} />
-        <Route exact path="/about" component={<About/>} />
-        <Route exact path="/contact" component={<Contact/>} />
-        <Route exact path="/legacy" component={<Legacy/>} />
+        <Route exact path="/">
+            <Home/>
+        </Route>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/about" component={(routerProps) => <AboutClass {...routerProps} />} />
+        <Route exact path="/contact" render={(renderProps)=><Contact {...renderProps} />} />
+        <Route exact path="/legacy" children={({match})=> (<Legacy {...match}/>)} />
     </Switch>
 )
 }
