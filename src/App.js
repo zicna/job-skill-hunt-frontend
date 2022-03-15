104635b8d0e9257f31973c6b69ebfaa829abb43c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-15 12:18:55 -0500

    add maincontainer to app.js, that will render FromContainer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom'
-import FormContainer from './containers/FormContainer'
 
 import Header from './pages/Header'
 import Footer from './pages/Footer'
+import MainContainer from './containers/MainContainer'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        < Header/>
-        <FormContainer />
+        <Header />
+        <MainContainer />
         <Footer />
       </Router>
     </div>
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import FormContainer from './FormContainer'
+
+const MainContainer = () => {
+  return (
+  <div className='container'>
+      <FormContainer />
+  </div>)
+}
+
+export default MainContainer
