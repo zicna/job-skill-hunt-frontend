556a99473bb4913202a221479ad2cc9030e0b0b4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-17 14:29:05 -0500

    include NavBar component into MainContainer

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 
 import FormContainer from './FormContainer'
 import RoutesContainer from './RoutesContainer'
 
 const MainContainer = () => {
   return (
-  <div className='container'>
+    <div className="container">
+      <NavBar />
       <FormContainer />
-      <RoutesContainer/>
-  </div>)
+      <RoutesContainer />
+    </div>
+  )
 }
 
 export default MainContainer
--- src/containers/RoutesContainer.js
+++ src/containers/RoutesContainer.js
@@ -3,7 +3,6 @@ import {Switch, Route, Redirect} from 'react-router-dom'
 
 import {loggedIn} from '../helpers/AuthHelpers'
 
-
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Contact from '../pages/Contact'
